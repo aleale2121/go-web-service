14cb0c55e804cdecb6ee48400251c7896efefffa
alefeweq <alefew@equally.ai>
2023-03-29 14:36:24 +0300

    make file and graceful shutdown added

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/ardanlabs/conf"
 	"github.com/dimfeld/httptreemux/v5"
 )
 
+var build = "develop"
+
 func main() {
-	conf.Parse([]string{""},"","")
+	_=conf.ErrHelpWanted
+	log.Println("Starting service ", build)
+	defer log.Println("service ended")
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+	log.Println("Stopping service")
 	httptreemux.New()
 }
