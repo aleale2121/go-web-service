a78a9af58db6dad7ff9522afdd22280c3203cf38
alefew <alefewyimer4@gmail.com>
2023-04-07 11:32:11 +0300

    initial seed

--- business/data/schema/schema.go
+++ business/data/schema/schema.go
@@ -6,14 +6,17 @@ import (
 	_ "embed" // Calls init function.
 	"fmt"
 
-	"github.com/ardanlabs/darwin"
 	"github.com/aleale2121/go-web-service/business/sys/database"
+	"github.com/ardanlabs/darwin"
 	"github.com/jmoiron/sqlx"
 )
 
 var (
 	//go:embed sql/schema.sql
 	schemaDoc string
+
+	//go:embed sql/seed.sql
+	seedDoc string
 
 	//go:embed sql/delete.sql
 	deleteDoc string
@@ -33,6 +36,28 @@ func Migrate(ctx context.Context, db *sqlx.DB) error {
 
 	d := darwin.New(driver, darwin.ParseMigrations(schemaDoc))
 	return d.Migrate()
+}
+
+// Seed runs the set of seed-data queries against db. The queries are ran in a
+// transaction and rolled back if any fail.
+func Seed(ctx context.Context, db *sqlx.DB) error {
+	if err := database.StatusCheck(ctx, db); err != nil {
+		return fmt.Errorf("status check database: %w", err)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	if _, err := tx.Exec(seedDoc); err != nil {
+		if err := tx.Rollback(); err != nil {
+			return err
+		}
+		return err
+	}
+
+	return tx.Commit()
 }
 
 // DeleteAll runs the set of Drop-table queries against db. The queries are ran in a
