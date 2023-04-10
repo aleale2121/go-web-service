abf47bcbb4bc715e91fbcb2de9f9eae19800029b
alefew <alefewyimer4@gmail.com>
2023-04-10 09:21:33 +0300

    support for starting and stoping container

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -13,14 +13,15 @@ import (
 	"time"
 
 	"github.com/aleale2121/go-web-service/app/services/sales-api/handlers"
-	"github.com/aleale2121/go-web-service/business/sys/database"
 	"github.com/aleale2121/go-web-service/business/sys/auth"
+	"github.com/aleale2121/go-web-service/business/sys/database"
 	"github.com/aleale2121/go-web-service/foundation/keystore"
+	"github.com/aleale2121/go-web-service/foundation/logger"
 	"github.com/ardanlabs/conf"
 	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
+
 /*
 Need to figure out timeouts for http service.
 */
@@ -30,7 +31,7 @@ var build = "develop"
 func main() {
 
 	// Construct the application logger.
-	log, err := initLogger("SALES-API")
+	log, err := logger.New("SALES-API")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -234,21 +235,4 @@ func run(log *zap.SugaredLogger) error {
 	}
 
 	return nil
-}
-
-func initLogger(service string) (*zap.SugaredLogger, error) {
-	config := zap.NewProductionConfig()
-	config.OutputPaths = []string{"stdout"}
-	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	config.DisableStacktrace = true
-	config.InitialFields = map[string]interface{}{
-		"service": service,
-	}
-
-	log, err := config.Build()
-	if err != nil {
-		return nil, err
-	}
-
-	return log.Sugar(), nil
 }
